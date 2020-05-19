27904b79fa5998e049834fdc6e233c106fbce771
crisan-andrada-30126 <61448551+crisan-andrada-30126@users.noreply.github.com>
2020-05-19 15:43:37 +0300

    Add files via upload

---
+++ LAb11Test/c3-sample/src/test/java/TestTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTest {
+    @Test
+    public void testCreate() {
+        System.out.println("Hello test");
+    }
+    }
